6fd177c029111492ca3dfe635f48ea38d944c553
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-03 18:14:18 -0300

    criando o layout principal e adicionando evento de click para o botao

--- app/src/main/java/br/com/borges/lucas/gastodeviagem/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/gastodeviagem/MainActivity.kt
@@ -2,10 +2,22 @@ package br.com.borges.lucas.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.borges.lucas.gastodeviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    binding.buttonCalculate.setOnClickListener{
+      calculate()
+    }
+  }
+
+  private fun calculate() {
+
   }
 }
