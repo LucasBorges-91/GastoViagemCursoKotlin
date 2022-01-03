72f3dddcb54cf0da3b3371eb189c83ac70d50a5f
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-03 19:00:01 -0300

    adicionando validacao dos valores informados e finalizando projeto

--- app/src/main/java/br/com/borges/lucas/gastodeviagem/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/gastodeviagem/MainActivity.kt
@@ -2,7 +2,9 @@ package br.com.borges.lucas.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.borges.lucas.gastodeviagem.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,12 +21,25 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun calculate() {
-    val distance = binding.editDistance.text.toString().toFloat()
-    val price = binding.editPrice.text.toString().toFloat()
-    val autonomy = binding.editAutonomy.text.toString().toFloat()
-
-    val totalValue = ( distance * price ) / autonomy
-    binding.textTotalValue.text = "R$ ${"%.2f".format( totalValue )}"
-
+    if ( validation() ) {
+      try {
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+        val totalValue = ( distance * price ) / autonomy
+        binding.textTotalValue.text = "R$ ${"%.2f".format( totalValue )}"
+      } catch ( ex: NumberFormatException ) {
+        Toast.makeText( this, "informe valores maiores que 0", Toast.LENGTH_LONG ).show()
+      }
+    } else {
+      Toast.makeText( this, "Preencha todos os campos", Toast.LENGTH_LONG ).show()
+    }
   }
+
+  private fun validation() = ( binding.editDistance.text.toString() != ""
+      && binding.editDistance.text.toString().toFloat() > 0
+      && binding.editPrice.text.toString() != ""
+      && binding.editPrice.text.toString().toFloat() > 0
+      && binding.editAutonomy.text.toString() != ""
+      && binding.editAutonomy.text.toString().toFloat() > 0 )
 }
